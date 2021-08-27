a5c20413c52dbcf067e82b7f0ef7d09255105ef0
OscarFlorez <66635225+Santiago-Florez@users.noreply.github.com>
2021-08-26 20:15:34 -0500

    Creacion clase View, y metodo FileChooser

---
+++ Workshop1/src/co/edu/unbosque/view/View.java
@@ -0,0 +1,24 @@
+package co.edu.unbosque.view;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class View {
+
+	public String chooserFile() {
+		String u = "";
+		try {
+			JOptionPane.showMessageDialog(null, "Busque el archivo .csv, que contiene la base de datos");
+			JFileChooser filechooser = new JFileChooser();
+			filechooser.showSaveDialog(filechooser);
+			File ubicacion = filechooser.getSelectedFile();
+			u = ubicacion.toString();
+		} catch (NullPointerException e) {
+			u = "Hubo un problema al obtener la ruta del archivo .csv";
+		}
+		return u;
+	}
+	
+}
